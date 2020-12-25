25f897a6a3ddef70f6d3861d79d4b0f958790442
wushan <w594533@163.com>
2020-12-25 19:32:09 +0800

    1

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,41 +7,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject
+class User extends Authenticatable implements  JWTSubject
 {
     use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password', 'account', 'openid', 'nickname', 'avatar', 'sex'
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    public static function findByOpenid($openid)
-    {
-        return self::where('openid', $openid)->first();
-    }
 
     public function getJWTIdentifier()
     {
@@ -52,4 +20,20 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'account', 'openid', 'nickname', 'avatar', 'sex'
+    ];
+
+    public static function findByOpenid($openid)
+    {
+        return self::where('openid', $openid)->first();
+    }
+
+    
 }
